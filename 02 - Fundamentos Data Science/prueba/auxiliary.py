564410146112dd905002911dfe6569ac0705bda2
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2019-07-18 23:59:41 -0400

    list hito 2 desafio 2

--- 02 - Fundamentos Data Science/prueba/auxiliary.py
+++ 02 - Fundamentos Data Science/prueba/auxiliary.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
 
 
 def binarize(df, column):
@@ -112,17 +113,22 @@ def dummierize_columns(data, columns):
 
     return df.drop(columns=new_columns).drop(columns=columns)
 
+
 def is_numeric(col):
     return (col.dtype == np.int64 or col.dtype == np.float64) and len(col.value_counts()) > 5
+
 
 def is_discrete(col):
     return not is_numeric(col)
 
+
 def is_binary(col):
     return len(col.value_counts()) == 2
 
+
 def is_nominal(col):
     return len(col.value_counts()) == 5
+
 
 def describe_columns(df):
     """Prints information about each column of the given dataframe.
@@ -143,43 +149,45 @@ def describe_columns(df):
         else:
             print(col.describe(), "\n")
 
+
 def humanize(col_name):
     t = {
-        'school' : 'Escuela del estudiante.',
-        'sex' : 'Sexo del estudiante',
-        'age' : 'Edad del estudiante',
-        'address' : 'Ubicación de la casa del estudiante',
-        'famsize' : 'Tamaño de la familia',
-        'Pstatus' : 'Estado cohabitacional de los padres',
-        'Medu' : 'Nivel educacional de la madre',
-        'Fedu' : 'Nivel educacional del padre',
-        'Mjob' : 'Ocupación de la madre',
-        'Fjob' : 'Ocupación del padre',
-        'reason' : 'Razón para escoger la escuela',
-        'guardian' : 'Apoderado del estudiante',
-        'traveltime' : 'Tiempo de viaje entre hogar y colegio',
-        'studytime' : 'Horas semanales dedicadas al estudio',
-        'failures' : 'Número de clases reprobadas',
-        'schoolsup' : 'Apoyo educacional del colegio',
-        'famsup' : 'Apoyo educacional familiar',
-        'paid' : 'Clases particulares pagadas',
-        'activities' : 'Actividades extracurriculares',
-        'nursery' : 'Asistió a guardería infantil',
-        'higher' : 'Desea proseguir estudios superiores',
-        'internet' : 'Acceso a internet desde el hogar',
-        'romantic' : 'Relación romántica',
-        'famrel' : 'Calidad de las relaciones familiares',
-        'freetime' : 'Tiempo libre fuera del colegio',
-        'goout' : 'Salidas con amigos',
-        'Dalc' : 'Consumo de alcohol en día de semana',
-        'Walc' : 'Consumo de alcohol en fines de semana',
-        'health' : 'Estado de salud actual',
-        'absences' : 'Cantidad de ausencias escolares',
-        'G1' : 'Notas durante el primer semestre',
-        'G2' : 'Notas durante el segundo semestre',
-        'G3' : 'Promedio final',
+        'school': 'Escuela del estudiante.',
+        'sex': 'Sexo del estudiante',
+        'age': 'Edad del estudiante',
+        'address': 'Ubicación de la casa del estudiante',
+        'famsize': 'Tamaño de la familia',
+        'Pstatus': 'Estado cohabitacional de los padres',
+        'Medu': 'Nivel educacional de la madre',
+        'Fedu': 'Nivel educacional del padre',
+        'Mjob': 'Ocupación de la madre',
+        'Fjob': 'Ocupación del padre',
+        'reason': 'Razón para escoger la escuela',
+        'guardian': 'Apoderado del estudiante',
+        'traveltime': 'Tiempo de viaje entre hogar y colegio',
+        'studytime': 'Horas semanales dedicadas al estudio',
+        'failures': 'Número de clases reprobadas',
+        'schoolsup': 'Apoyo educacional del colegio',
+        'famsup': 'Apoyo educacional familiar',
+        'paid': 'Clases particulares pagadas',
+        'activities': 'Actividades extracurriculares',
+        'nursery': 'Asistió a guardería infantil',
+        'higher': 'Desea proseguir estudios superiores',
+        'internet': 'Acceso a internet desde el hogar',
+        'romantic': 'Relación romántica',
+        'famrel': 'Calidad de las relaciones familiares',
+        'freetime': 'Tiempo libre fuera del colegio',
+        'goout': 'Salidas con amigos',
+        'Dalc': 'Consumo de alcohol en día de semana',
+        'Walc': 'Consumo de alcohol en fines de semana',
+        'health': 'Estado de salud actual',
+        'absences': 'Cantidad de ausencias escolares',
+        'G1': 'Notas durante el primer semestre',
+        'G2': 'Notas durante el segundo semestre',
+        'G3': 'Promedio final',
     }
     return t[col_name]
+
 
 def plot_columns_behaviour(df, kind='countplot'):
     cols = list(df.columns)
@@ -200,8 +208,34 @@ def plot_columns_behaviour(df, kind='countplot'):
             sns.distplot(df[col_name], rug=True)
             plt.title(humanize(col_name))
             plt.xlabel("")
-            plt.axvline(df[col_name].mean(), color='tomato', linestyle='--', label='mean')
-            plt.axvline(df[col_name].median(), color='green', linestyle='--', label='median')
+            plt.axvline(df[col_name].mean(), color='tomato',
+                        linestyle='--', label='mean')
+            plt.axvline(df[col_name].median(), color='green',
+                        linestyle='--', label='median')
             plt.legend()
         plt.tight_layout()
 
+
+def run_hip_test(df, variable, binary):
+    values = df[binary].unique()
+    group_1 = df[df[binary] == values[1]][variable].dropna()
+    group_0 = df[df[binary] == values[0]][variable].dropna()
+    t, pval = stats.ttest_ind(group_1, group_0)
+    return abs(t) > 1.96 and pval < 0.05
+
+
+def graph_hist(df, variable, binary):
+    values = df[binary].unique()
+    group_1 = df[df[binary] == values[1]][variable].dropna()
+    group_0 = df[df[binary] == values[0]][variable].dropna()
+
+    def draw(group, group_name, color):
+        plt.hist(group, alpha=0.5, color=color, density=True,
+                 label='{} {}'.format(binary, group_name))
+        plt.axvline(np.mean(group), color=color)
+
+    draw(group_1, values[1], 'tomato')
+    draw(group_0, values[0], 'lightblue')
+    plt.title('{} | {}'.format(binary, variable))
+    plt.legend(loc='best')
+    plt.show()
